df70c49a0322f005356de658d3c2e05e9f76369d
xela10104 <aglaeser@mail.csuchico.edu>
2015-05-07 09:49:04 -0700

    Deleted excess scripts. Added navmesh Project.

---
+++ Nav_Mesh/Assets/Scrpts/Archer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Archer: MonoBehaviour {
+
+	public float Health = 100.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
